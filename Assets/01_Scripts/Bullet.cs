9810bd77697d97435fe3cdad81dc741ecfc9bf25
Pablo Guti <pablogutierrrez0@gmail.com>
2024-10-07 13:48:30 -0400

    Inicio proyecto

---
+++ Assets/01_Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5;
+    public float damage = 1;
+    public float timeToDestroy = 4;
+    public bool playerBullet = false;
+
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && playerBullet)
+        {
+            Enemy e = collision.gameObject.GetComponent<Enemy>();
+            e.TakeDamage(damage);
+            Destroy(gameObject);
+        }else if (collision.gameObject.CompareTag("Player") && !playerBullet)
+        {
+            Player p = collision.gameObject.GetComponent<Player>();
+            p.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
